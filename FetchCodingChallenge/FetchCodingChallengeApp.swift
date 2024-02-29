507633ad2c5a406390474f4f85c117b6a0c9ee24
Carson Gross <carsongro@gmail.com>
2024-02-29 11:22:08 -0700

    Initial Commit

---
+++ FetchCodingChallenge/FetchCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchCodingChallengeApp.swift
+//  FetchCodingChallenge
+//
+//  Created by Carson Gross on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
